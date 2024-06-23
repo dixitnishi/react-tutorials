ff76bd92734123a81ae60566a99668e17eda038b
nishi <dixit.nishi0409@gmail.com>
2024-06-23 17:35:53 +0530

    quiz application

---
+++ quiz-application/src/components/Quiz.jsx
@@ -0,0 +1,41 @@
+import React, { act, useCallback, useRef } from "react";
+import { useState } from "react";
+import QUESTIONS from "../questions.js";
+import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
+
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswers((prevAnswer) => {
+      return [...prevAnswer, selectedAnswer];
+    });
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
+
+  if (quizIsComplete) {
+    return <Summary userAnswers={userAnswers} />;
+  }
+
+  return (
+    <div id="quiz">
+      <Question
+        key={activeQuestionIndex}
+        questionIndex={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkpAnswer={handleSkipAnswer}
+      />
+    </div>
+  );
+}
